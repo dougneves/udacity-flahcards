14056aa9aa0d4e028ce944d351fa9aabbbc28b09
Douglas Neves <dougneves@gmail.com>
2018-05-27 02:03:09 -0300

    adding StackNavigator

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import Decks from './components/decks';
+
+const Stack = createStackNavigator({
+  Decks: {
+    screen: Decks
+  }
+});
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <Stack />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
